f3933e5ce625d704e272406af968c8fd6b7594f3
Ilia Lesik <ilialesik@gmail.com>
2019-03-31 12:23:09 +0300

    Add unit test for useTrigger

---
+++ src/__tests__/useTrigger.test.js
@@ -0,0 +1,33 @@
+import React, { useEffect } from "react";
+import { render, wait } from "react-testing-library";
+import { createTrigger } from "../createTrigger";
+import { useTrigger } from "../useTrigger";
+
+describe("useTrigger", () => {
+    it("call trigger", async () => {
+        const trigger = createTrigger();
+        const effect = jest.fn();
+
+        const Component = () => {
+            const triggerValue = useTrigger(trigger);
+
+            useEffect(() => {
+                effect();
+            }, [triggerValue]);
+
+            return <div />;
+        };
+
+        const { container, rerender } = render(<Component />);
+
+        await wait(() => {
+            trigger();
+        });
+
+        await wait(() => {
+            rerender(<Component />);
+
+            expect(effect.mock.calls.length).toEqual(2);
+        });
+    });
+});
